bc7ea13340611edbc496d6fa4e141f334bff8e69
shadrach256 <shadrachssozi18@gmail.com>
2025-04-18 21:56:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    my_pet = Pet("Fido")
+    
+    my_pet.get_status()
+    
+    my_pet.eat()
+    my_pet.play()
+    my_pet.sleep()
+    
+    my_pet.get_status()
+    
+    my_pet.train("sit")
+    my_pet.train("roll over")
+    my_pet.show_tricks()
